6a5bad2b4bba02e08bdba6e5a85becfcb5b4fbe8
Ahmed Eldeeb <eldeeb757575@gmail.com>
2023-08-28 00:52:45 +0300

    pushing header file

---
+++ monty.h
@@ -0,0 +1,48 @@
+#ifndef MONTY_H
+#define MONTY_H
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct stack_s
+{
+		int n;
+		struct stack_s *prev;
+		struct stack_s *next;
+} stack_t;
+
+/**
+ * struct Stack - struct to access top node and store stack size
+ * @top: pointer to the top node
+ * @size: number of nodes in the stack
+ */
+typedef struct Stack
+{
+	stack_t *top;
+	int size;
+} stack;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+typedef struct instruction_s
+{
+		char *opcode;
+		void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+
+void checkArguments(int num_args, char **list_args);
+int  checkFile(char *file);
+#endif
